d4271b8aaf3e392a73018c186ffe561dc9923e19
sudhagarmsd5 <54501573+sudhagarmsd5@users.noreply.github.com>
2023-01-27 15:11:23 +0530

    displayed google maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { GoogleMap, MapMarker } from '@angular/google-maps';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  add_btn: boolean = true;
+  del_btn: boolean = true;
+  add_marker: boolean = false;
+
+  currentLatitude: number;
+  currentLongitude: number;
+  center: any;
+
+  map_form: FormGroup;
+
+  zoom = 4;
+  markerOptions: any = { draggable: false };
+  markerPositions: any[] = [];
+
+  @ViewChild('map') map: google.maps.Map;
+
+  constructor() {
+    this.map_form = new FormGroup({
+      location_name: new FormControl('', [Validators.required]),
+    });
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.currentLatitude = position.coords.latitude;
+      this.currentLongitude = position.coords.longitude;
+      console.log(position);
+      this.center = { lat: this.currentLatitude, lng: this.currentLongitude };
+    });
+
+    this.map_form.controls['location_name'].valueChanges.subscribe((val) => {
+      console.log(val);
+      if (val !== '') {
+        this.add_btn = false;
+        // this.add_marker = true;
+      } else {
+        this.add_btn = true;
+        // this.add_marker = false;
+      }
+    });
+  }
+
+  addMarker(event: any) {
+    if (!this.add_btn) {
+      this.markerPositions.push(event.latLng.toJSON());
+    }
+    // this.add_marker = false;
+    // console.log(event.latLng.toJSON());
+  }
+
+  addLocation() {
+    this.add_marker = true;
+  }
+
+  clearMarkers() {
+    // this.markerPositions = [];
+  }
+
+  deleteLocation() {}
+  ngDoCheck() {
+    // console.log(this.map.googleMap.controls);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './shared/material.module';
 import { GoogleMapsModule } from '@angular/google-maps';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MaterialModule, GoogleMapsModule],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    GoogleMapsModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
